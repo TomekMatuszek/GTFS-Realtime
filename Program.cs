6bbc9e8c131fcb978ca3b78bb9eb2119637c6130
Tomek Matuszek <tom.mateuszek@gmail.com>
2022-12-11 13:15:06 +0100

    exclude token, count records

--- Program.cs
+++ Program.cs
@@ -11,9 +11,10 @@ namespace poznan_GTFS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("GTFS-RT data for public transport vehicles in Poznan ------------------------");
 
-            var HttpRequest = (HttpWebRequest)WebRequest.Create("https://www.ztm.poznan.pl/pl/dla-deweloperow/getGtfsRtFile/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZXN0Mi56dG0ucG96bmFuLnBsIiwiY29kZSI6MSwibG9naW4iOiJtaFRvcm8iLCJ0aW1lc3RhbXAiOjE1MTM5NDQ4MTJ9.ND6_VN06FZxRfgVylJghAoKp4zZv6_yZVBu_1-yahlo&file=trip_updates.pb");
+            //var token = "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZXN0Mi56dG0ucG96bmFuLnBsIiwiY29kZSI6MSwibG9naW4iOiJtaFRvcm8iLCJ0aW1lc3RhbXAiOjE1MTM5NDQ4MTJ9.ND6_VN06FZxRfgVylJghAoKp4zZv6_yZVBu_1-yahlo&";
+            var HttpRequest = (HttpWebRequest)WebRequest.Create($"https://www.ztm.poznan.pl/pl/dla-deweloperow/getGtfsRtFile/?file=trip_updates.pb");
             TransitRealtime.FeedMessage feed;
             using (var response = (HttpWebResponse)HttpRequest.GetResponse())
             {
@@ -24,6 +25,7 @@ namespace poznan_GTFS
                 HttpRequest.Abort();
             }
 
+            Console.WriteLine(feed.Entity.Count);
             Console.WriteLine(feed.Entity[0].ToString());
             Console.WriteLine(feed.Entity[0].TripUpdate.Vehicle.Label);
             Console.WriteLine(feed.Entity[0].TripUpdate.StopTimeUpdate[0].Arrival.Delay);
