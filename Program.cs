7047b14f0e7e340f9c2821c058b2e6beba51389e
Tomek Matuszek <tom.mateuszek@gmail.com>
2022-12-12 02:16:37 +0100

    converting Feed into DataTable

--- Program.cs
+++ Program.cs
@@ -15,16 +15,19 @@ namespace GTFS_parser
             Console.WriteLine("GTFS-RT data for public transport vehicles in Poznan ------------------------");
 
             var tasks = new Tasks();
-            var feed = tasks.DownloadGTFS("trip_updates");
-            tasks.PrintGTFS(feed);
+            var feed = tasks.DownloadGTFS("vehicle_positions");
+            //tasks.PrintGTFS(feed);
+
+            var results = tasks.PrepareData(feed);
+            Console.WriteLine(results.Rows.Count);
 
             Console.WriteLine(feed.Entity.Count);
-            Console.WriteLine(feed.Entity[0].ToString());
-            Console.WriteLine(feed.Entity[0].TripUpdate.Vehicle.Label);
-            Console.WriteLine(feed.Entity[0].TripUpdate.StopTimeUpdate[0].Arrival.Delay);
+            Console.WriteLine(feed.Entity[2].ToString());
+            Console.WriteLine(feed.Entity[2].TripUpdate.Vehicle.Label);
+            Console.WriteLine(feed.Entity[2].TripUpdate.StopTimeUpdate[0].Arrival.Delay);
 
             DateTime date = new DateTime(1970, 1, 1, 0, 0, 0).ToLocalTime();
-            date = date.AddSeconds(feed.Entity[0].TripUpdate.Timestamp);
+            date = date.AddSeconds(feed.Entity[6].TripUpdate.Timestamp);
             Console.WriteLine(date);
 
             Console.ReadLine();
--- Tasks.cs
+++ Tasks.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.SqlServer.Types;
 
 namespace GTFS_parser
 {
     public class Tasks
     {
+        DataTable Data = new DataTable();
+
         public TransitRealtime.FeedMessage DownloadGTFS(string type)
         {
             //var token = "token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ0ZXN0Mi56dG0ucG96bmFuLnBsIiwiY29kZSI6MSwibG9naW4iOiJtaFRvcm8iLCJ0aW1lc3RhbXAiOjE1MTM5NDQ4MTJ9.ND6_VN06FZxRfgVylJghAoKp4zZv6_yZVBu_1-yahlo&";
@@ -22,7 +25,6 @@ namespace GTFS_parser
                 response.Close();
                 responseStream.Close();
                 HttpRequest.Abort();
-                Console.WriteLine(feed.Entity.Count);
                 return feed;
             }
         }
@@ -33,6 +35,60 @@ namespace GTFS_parser
             {
                 Console.WriteLine($"{feed.Entity[i].TripUpdate.Vehicle.Label} | {feed.Entity[i].TripUpdate.StopTimeUpdate[0].Arrival.Delay}");
             }
+        }
+
+        public DataTable PrepareData(TransitRealtime.FeedMessage feed)
+        {
+            Data = PrepareTable();
+            for (int i = 0; i < feed.Entity.Count; i++)
+            {
+                Data = FillTable(Data, feed.Entity[i]);
+            }
+            return Data;
+        }
+
+        private DataTable PrepareTable()
+        {
+            var table = Data;
+            table = AddColumn(table, "System.String", "trip_id");
+            table = AddColumn(table, "System.String", "line");
+            table = AddColumn(table, "System.String", "brigade");
+            table = AddColumn(table, "System.Double", "position_x");
+            table = AddColumn(table, "System.Double", "position_y");
+            table = AddColumn(table, "System.Double", "speed");
+            table = AddColumn(table, "System.DateTime", "time");
+            table = AddColumn(table, "System.Int32", "delay");
+            return table;
+        }
+
+        private DataTable AddColumn(DataTable table, string type, string name)
+        {
+            DataColumn column;
+            if (type == "SqlGeography")
+            {
+                column = new DataColumn(dataType: typeof(SqlGeography), columnName: name);
+            }
+            else
+            {
+                column = new DataColumn(dataType: Type.GetType(type), columnName: name);
+            }
+            table.Columns.Add(column);
+            return table;
+        }
+
+        private DataTable FillTable(DataTable table, TransitRealtime.FeedEntity obj)
+        {
+            var row = table.NewRow();
+            row["trip_id"] = obj.Vehicle.Trip.TripId;
+            row["line"] = obj.Vehicle.Trip.RouteId;
+            row["brigade"] = obj.Vehicle.Vehicle.Label;
+            row["position_x"] = obj.Vehicle.Position.Longitude;
+            row["position_y"] = obj.Vehicle.Position.Latitude;
+            row["speed"] = obj.Vehicle.Position.Speed;
+            DateTime date = new DateTime(1970, 1, 1, 0, 0, 0).ToLocalTime();
+            row["time"] = date.AddSeconds(obj.Vehicle.Timestamp);
+            table.Rows.Add(row);
+            return table;
         }
     }
 }
